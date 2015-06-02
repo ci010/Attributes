1f4799b8bfaae8bc3e7f9815662a94200beb2254
ci010 <18211182378@163.com>
2015-06-01 21:33:29 -0700

    add some event

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/CommonHandler.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/CommonHandler.java
@@ -1,9 +1,14 @@
 package net.ci010.attributesmod.handler;
 
+import net.ci010.attributesmod.properties.Status;
 import net.ci010.attributesmod.properties.dynamic.Sleepness;
 import net.ci010.attributesmod.properties.dynamic.Strength;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class CommonHandler
@@ -14,5 +19,64 @@ public class CommonHandler
 		if (event.entity instanceof EntityPlayer && Sleepness.get((EntityPlayer) event.entity) == null)
 			Sleepness.register((EntityPlayer)event.entity);
 			Strength.register((EntityPlayer)event.entity);
+	}
+	
+	@SubscribeEvent
+	public void jumpEvent(LivingJumpEvent event)
+	{
+		if(event.entityLiving instanceof EntityPlayer)
+		{
+			Sleepness playerSl = Sleepness.get((EntityPlayer)event.entityLiving);
+			Strength playerSt = Strength.get((EntityPlayer)event.entityLiving);
+			
+			playerSl.consume(true);
+			playerSt.consume(false);
+		}
+	}
+	
+	@SubscribeEvent
+	public void LivingUpdate(LivingUpdateEvent event)
+	{
+		if(event.entityLiving.isDead)
+		{
+			return;
+		}
+		if(event.entityLiving instanceof EntityPlayer)
+		{
+			Sleepness playerSl = Sleepness.get((EntityPlayer)event.entityLiving);
+			Strength playerSt = Strength.get((EntityPlayer)event.entityLiving);
+			
+			Status.timer++;
+			if(Status.timer>20)
+			{
+				Status.timer = 0;
+				playerSl.consume(false);
+				playerSt.consume(false);
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void breakEvent(BreakSpeed event)
+	{
+		Strength playerSt = Strength.get(event.entityPlayer);
+		Sleepness playerSl = Sleepness.get(event.entityPlayer);
+		
+		if(playerSt.getCurrent()<0||playerSl.getCurrent()<0)
+		{
+			event.newSpeed = event.originalSpeed/2;
+		}
+	}
+	
+	@SubscribeEvent
+	public void attackEvent(LivingAttackEvent event)
+	{
+		Strength playerSt = Strength.get((EntityPlayer)event.entityLiving);
+		Sleepness playerSl = Sleepness.get((EntityPlayer)event.entityLiving);
+		
+		if(playerSt.getCurrent()<0||playerSl.getCurrent()<0)
+		{
+			
+		}
 	}
 }
