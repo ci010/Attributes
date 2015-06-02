7c6208dab127709750d128daeb0ac903c5ce9d5e
ci010 <18211182378@163.com>
2015-06-02 12:48:42 -0700

    register the FMLHandler

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/CommonProxy.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/CommonProxy.java
@@ -3,9 +3,11 @@ package net.ci010.attributesmod.proxy;
 import net.ci010.attributesmod.Resource;
 import net.ci010.attributesmod.gui.SleepnessBar;
 import net.ci010.attributesmod.handler.CommonHandler;
+import net.ci010.attributesmod.handler.FMLHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy
@@ -15,6 +17,7 @@ public class CommonProxy
 	{
 		MinecraftForge.EVENT_BUS.register(new SleepnessBar(Minecraft.getMinecraft()));
 		MinecraftForge.EVENT_BUS.register(new CommonHandler());
+		FMLCommonHandler.instance().bus().register(new FMLHandler());
 	}
 	public void iniConfig(FMLPreInitializationEvent event)
 	{
