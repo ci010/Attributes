ec0ead103e63b4a5a9d400a7f765f8ebc386dbc9
ci010 <18211182378@163.com>
2015-06-04 11:38:26 -0700

    name changed

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/CommonProxy.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/CommonProxy.java
@@ -3,7 +3,7 @@ package net.ci010.attributesmod.proxy;
 import net.ci010.attributesmod.Resource;
 import net.ci010.attributesmod.gui.SleepnessBar;
 import net.ci010.attributesmod.handler.CommonHandler;
-import net.ci010.attributesmod.handler.FMLHandler;
+import net.ci010.attributesmod.handler.TalentHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
@@ -17,13 +17,11 @@ public class CommonProxy
 	{
 		MinecraftForge.EVENT_BUS.register(new SleepnessBar(Minecraft.getMinecraft()));
 		MinecraftForge.EVENT_BUS.register(new CommonHandler());
-		FMLCommonHandler.instance().bus().register(new FMLHandler());
+		FMLCommonHandler.instance().bus().register(new TalentHandler());
 	}
 	public void iniConfig(FMLPreInitializationEvent event)
 	{
 		Resource.config = new Configuration(event.getSuggestedConfigurationFile());
 		Resource.config.load();
 	}
-	
-
 }
