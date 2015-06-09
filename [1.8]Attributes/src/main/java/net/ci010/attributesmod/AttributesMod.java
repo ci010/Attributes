da0bc4616d7e56ae4f5ede361d4a7404c8165216
ci010 <18211182378@163.com>
2015-06-09 15:08:10 -0700

    fix AttibuteMod Class

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
@@ -2,7 +2,11 @@ package net.ci010.attributesmod;
 
 import net.ci010.attributesmod.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(name = AttributesMod.NAME, modid = AttributesMod.MODID, version = AttributesMod.VERSION)
 public class AttributesMod
@@ -14,5 +18,21 @@ public class AttributesMod
 	@SidedProxy(clientSide = "net.ci010.attributesmod.proxy.ClientProxy", serverSide = "net.ci010.attributesmod.proxy.CommonProxy")
 	CommonProxy proxy;
 	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) 
+	{
+		
+	}
 	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
 }
