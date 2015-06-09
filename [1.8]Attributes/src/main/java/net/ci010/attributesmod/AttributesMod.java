3d3b75aa5bf4523120c82aa81348852e08618aaa
ci010 <18211182378@163.com>
2015-06-09 15:06:24 -0700

    update

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
@@ -1,6 +1,8 @@
 package net.ci010.attributesmod;
 
+import net.ci010.attributesmod.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 
 @Mod(name = AttributesMod.NAME, modid = AttributesMod.MODID, version = AttributesMod.VERSION)
 public class AttributesMod
@@ -8,6 +10,9 @@ public class AttributesMod
 	public static final String MODID = "attributes";
 	public static final String NAME = "Attributes";
 	public static final String VERSION = "1.0";
+
+	@SidedProxy(clientSide = "net.ci010.attributesmod.proxy.ClientProxy", serverSide = "net.ci010.attributesmod.proxy.CommonProxy")
+	CommonProxy proxy;
 	
-	@SideProxy()
+	
 }
