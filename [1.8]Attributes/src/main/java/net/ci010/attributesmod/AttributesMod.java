12ee08c46d62673bea853f1b6f2fcb27a3f234ff
ci010 <18211182378@163.com>
2015-06-09 15:03:27 -0700

    fix 1

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
@@ -2,10 +2,12 @@ package net.ci010.attributesmod;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(name=,modid=,)
+@Mod(name = AttributesMod.NAME, modid = AttributesMod.MODID, version = AttributesMod.VERSION)
 public class AttributesMod
 {
 	public static final String MODID = "attributes";
 	public static final String NAME = "Attributes";
-	public static final String 
+	public static final String VERSION = "1.0";
+	
+	@SideProxy()
 }
