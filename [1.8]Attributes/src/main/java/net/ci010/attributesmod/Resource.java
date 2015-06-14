f10435b41646571e01bcdbe1b81dea0225ee60b5
ci010 <18211182378@163.com>
2015-06-14 08:29:27 -0700

    update the resource

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/Resource.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/Resource.java
@@ -1,10 +1,14 @@
 package net.ci010.attributesmod;
 
+import java.util.Random;
+
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.config.Configuration;
 
 public class Resource
 {
+	public static final Random r = new Random();
+	
 	public static Configuration config;
 	
 	public static final ResourceLocation sleepTexturepath = new ResourceLocation("attributes", "textures/gui/sleep_bar.png");
@@ -36,4 +40,13 @@ public class Resource
 		return 0;
 	}
 
+	public static int getPowerFactor()
+	{
+		return config.getInt("PowerFactor", "", 1, 1, 100, "use as power factor");
+	}
+
+	public static float getFactor(String id)
+	{
+		return 0;
+	}
 }
