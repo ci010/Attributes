d68b66edbe2b20baf77e375f46d7c775fd7537b6
ci010 <18211182378@163.com>
2015-06-14 08:28:56 -0700

    clear the code

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/skills/Swimming.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/skills/Swimming.java
@@ -2,13 +2,7 @@ package net.ci010.attributesmod.properties.skills;
 
 import net.ci010.attributesmod.properties.Attributes;
 
-public class Swimming extends Attributes
+public class Swimming
 {
-
-	public Swimming(int value)
-	{
-		super(value);
-		// TODO 自动生成的构造函数存根
-	}
 
 }
