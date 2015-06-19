fcbf93dbbd42af30886b2dedde7bf4b6ab5d930c
ci010 <18211182378@163.com>
2015-06-19 01:57:06 -0700

    add getPlayer (client)

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/ClientProxy.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/ClientProxy.java
@@ -2,22 +2,39 @@ package net.ci010.attributesmod.proxy;
 
 import net.ci010.attributesmod.gui.SleepnessBar;
 import net.ci010.attributesmod.handler.CommonHandler;
+import net.ci010.attributesmod.handler.GuiEventHandler;
+import net.ci010.attributesmod.handler.KeybindingHandler;
 import net.ci010.attributesmod.handler.TalentHandler;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 
 
 public class ClientProxy extends CommonProxy
 {
-	//client proxy will do some extra things only working on client side.
 	@Override
 	public void iniHandler()
 	{
-		MinecraftForge.EVENT_BUS.register(new CommonHandler());
-		FMLCommonHandler.instance().bus().register(new TalentHandler());
+		super.iniHandler();
 		MinecraftForge.EVENT_BUS.register(new SleepnessBar(Minecraft.getMinecraft()));
+		FMLCommonHandler.instance().bus().register(new KeybindingHandler());
+		MinecraftForge.EVENT_BUS.register(new GuiEventHandler());
+	}
+
+	@Override
+	public EntityPlayer getPlayerEntity(MessageContext ctx) 
+	{
+		return Minecraft.getMinecraft().thePlayer;
+	 // Note that if you simply return 'Minecraft.getMinecraft().thePlayer',
+	 // your packets will not work because you will be getting a client
+	 // player even when you are on the server! Sounds absurd, but it's true.
+
+	 // Solution is to double-check side before returning the player:
+	 //return (ctx.side.isClient() ? Minecraft.getMinecraft().thePlayer : super.getPlayerEntity(ctx));
 	}
 	
 
