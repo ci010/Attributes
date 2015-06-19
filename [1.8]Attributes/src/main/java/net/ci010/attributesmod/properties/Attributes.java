715890027c4e3af7f2ef2c27755ef201bcdf1590
ci010 <18211182378@163.com>
2015-06-19 01:56:25 -0700

    make set and get for attributes of a player

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/Attributes.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/Attributes.java
@@ -2,11 +2,14 @@ package net.ci010.attributesmod.properties;
 
 import net.ci010.attributesmod.handler.TalentHandler;
 import net.ci010.attributesmod.properties.basic.*;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.StatBase;
 import net.minecraft.stats.StatList;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * 
@@ -15,19 +18,20 @@ import net.minecraft.stats.StatList;
  */
 public abstract class Attributes
 {
-	public static final Agility agilityInstance = new Agility("AGILITY");
-	public static final Endurance enduranceInstance = new Endurance("ENDURANCE");
-	public static final Power powerInstance = new Power("POWER");
+	public static Agility agilityInstance = new Agility("AGILITY");
+	public static Endurance enduranceInstance = new Endurance("ENDURANCE");
+	public static Power powerInstance = new Power("POWER");
 
 	public final float factor[];
 
-	protected String id = "dummy";
+	public String id = "Attributes";
 
 	protected Attributes(String id, float... factor)
 	{
 		this.id = id;	
 		this.factor = factor;
 	}
+
 
 	/**
 	 * Update player's attribute by the statistic value of player
@@ -42,7 +46,7 @@ public abstract class Attributes
 
 		int limit = TalentHandler.getLimit(playerData).getInteger(this.id);
 		
-		playerData.setInteger(this.id, affectByTalent(talent, limit, player));
+		playerData.getCompoundTag("ATTRIBUTES").setInteger(this.id, affectByTalent(talent, limit, player));
 	}
 
 	protected abstract int affectByTalent(int upgradeTalent, int limitTalent, EntityPlayerMP player);
@@ -54,17 +58,24 @@ public abstract class Attributes
      */
 	protected final float getMultiplier(EntityPlayer player)
 	{
-		NBTTagCompound playerData = player.getEntityData();
-		int attribute = playerData.getInteger(this.id);
+		int attribute = getAttributes(player).getInteger(this.id);
 		return this.transformToPerformance(attribute);
 	}
 
 	protected abstract float transformToPerformance(int attribute);
 	
+	
+
+	public int getAttribute(EntityPlayer player)
+	{
+		return getAttributes(player).getInteger(this.id);
+	}
+	
 	/**
 	 * Update all the attributes of the player
 	 * @param player The player needed to be updated attributes
 	 * */
+	
 	public static final void updatePlayer(EntityPlayer player)
 	{
 		if (player instanceof EntityPlayerMP)
@@ -75,5 +86,37 @@ public abstract class Attributes
 			Attributes.enduranceInstance.upgrade(playerMP);
 			Attributes.powerInstance.upgrade(playerMP);
 		}
+	}
+	
+
+	public static final NBTTagCompound getAttributes(EntityPlayer player)
+	{
+//		if(player instanceof EntityPlayerSP)
+//		{
+//			System.out.println("getAttributes is SP");
+//		}
+//		if(player instanceof EntityPlayerMP)
+//		{
+//			System.out.println("getAttributes is MP");
+//		}
+		return player.getEntityData().getCompoundTag("ATTRIBUTES");
+	}
+	
+	public static final void setAttributes(EntityPlayer player, NBTTagCompound data)
+	{
+		if(player instanceof EntityPlayerSP)
+		{
+			System.out.println("setAttributes is SP");
+		}
+		if(player instanceof EntityPlayerMP)
+		{
+			System.out.println("setAttributes is MP");
+		}
+		if(player == null)
+		{
+			System.out.println("player is null");
+			return;
+		}
+		player.getEntityData().setTag("ATTRIBUTES",data);
 	}
 }
