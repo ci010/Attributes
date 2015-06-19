e2d05113cc534485f8b9cc9780d5e3be5921aaba
ci010 <18211182378@163.com>
2015-06-19 01:57:37 -0700

    register new handler. add get player(server)

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/CommonProxy.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/proxy/CommonProxy.java
@@ -1,20 +1,27 @@
 package net.ci010.attributesmod.proxy;
 
+import net.ci010.attributesmod.AttributesMod;
 import net.ci010.attributesmod.Resource;
+import net.ci010.attributesmod.gui.GuiHandler;
 import net.ci010.attributesmod.handler.*;
+import net.ci010.attributesmod.network.PacketDispatcher;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class CommonProxy
 {
-	// Common proxy will register the handlers(anything...) working for both
-	// server and client side.
 	public void iniHandler()
 	{
 		MinecraftForge.EVENT_BUS.register(new CommonHandler());
+		MinecraftForge.EVENT_BUS.register(new DebugHandler());
 		FMLCommonHandler.instance().bus().register(new TalentHandler());
+		NetworkRegistry.INSTANCE.registerGuiHandler(AttributesMod.instance, new GuiHandler());
+		PacketDispatcher.registerPackets();
 	}
 
 	public void iniConfig(FMLPreInitializationEvent event)
@@ -31,4 +38,8 @@ public class CommonProxy
 	// return ((EntityPlayerMP)event.entityLiving).getStatFile().readStat(stat);
 	// }
 
+	public EntityPlayer getPlayerEntity(MessageContext ctx) 
+	{
+		 return ctx.getServerHandler().playerEntity;
+	}
 }
