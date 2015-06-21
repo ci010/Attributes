680964540093280d8a77fb9a94bfc2596d186cea
ci010 <18211182378@163.com>
2015-06-20 19:49:08 -0700

    clear up

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/GuiEventHandler.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/GuiEventHandler.java
@@ -1,8 +1,11 @@
 package net.ci010.attributesmod.handler;
 
 import net.ci010.attributesmod.gui.AttributeInventory;
-import net.minecraft.client.Minecraft;
+import net.ci010.attributesmod.network.PacketDispatcher;
+import net.ci010.attributesmod.network.SyncAttributesMessage;
 import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
