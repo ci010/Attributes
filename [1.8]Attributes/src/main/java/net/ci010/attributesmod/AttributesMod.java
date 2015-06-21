ab26e08538f89509cb3d5ae7dcb83dee1d53f81d
ci010 <18211182378@163.com>
2015-06-21 02:46:46 -0700

    register new command

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/AttributesMod.java
@@ -1,6 +1,8 @@
 package net.ci010.attributesmod;
 
+import net.ci010.attributesmod.command.AttribueCommand;
 import net.ci010.attributesmod.proxy.CommonProxy;
+import net.minecraft.command.ServerCommandManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -8,6 +10,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(name = AttributesMod.NAME, modid = AttributesMod.MODID, version = AttributesMod.VERSION)
 public class AttributesMod
@@ -16,14 +19,12 @@ public class AttributesMod
 	public static final String NAME = "Attributes";
 	public static final String VERSION = "1.0";
 
-	
-	
 	@SidedProxy(serverSide = "net.ci010.attributesmod.proxy.CommonProxy", clientSide = "net.ci010.attributesmod.proxy.ClientProxy")
 	public static CommonProxy proxy;
 
 	@Instance("Attributes")
 	public static AttributesMod instance;
-	
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
@@ -41,5 +42,12 @@ public class AttributesMod
 	public void postInit(FMLPostInitializationEvent event)
 	{
 
+	}
+
+	@EventHandler
+	public void serverStarting(FMLServerStartingEvent event)
+	{
+		ServerCommandManager serverCommandManager = (ServerCommandManager) event.getServer().getCommandManager();
+		serverCommandManager.registerCommand(new AttribueCommand());
 	}
 }
