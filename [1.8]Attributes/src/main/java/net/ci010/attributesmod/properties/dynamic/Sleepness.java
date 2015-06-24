3d6f8e27e0d9392367bb4d7520ea6cf0e5c78138
ci010 <18211182378@163.com>
2015-06-24 23:13:56 +0800

    allocate the value dynamicly

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/dynamic/Sleepness.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/dynamic/Sleepness.java
@@ -1,32 +1,34 @@
 package net.ci010.attributesmod.properties.dynamic;
 
+import net.ci010.attributesmod.Resource;
 import net.ci010.attributesmod.properties.Status;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IExtendedEntityProperties;
 
 public class Sleepness extends Status implements IExtendedEntityProperties
 {
 	// NAME is used to register extended property
-	public static final String NAME = "Sleepness";
 	
 	public boolean isSleeping;
 
 	public Sleepness(EntityPlayer player)
 	{
 		super(player);
-		this.id = "Sleepness";
-		this.dataId = 20;
-		this.player.getDataWatcher().addObject(dataId, this.max);
-	}
-
-	public static final void register(EntityPlayer player)
-	{
-		System.out.println("register player prop");
-		player.registerExtendedProperties(NAME, new Sleepness(player));
+		this.id = "sleepness";
+		this.max = Resource.maxOfSl;
+		
 	}
 
 	public static final Sleepness get(EntityPlayer player)
 	{
-		return (Sleepness) player.getExtendedProperties(NAME);
+		return (Sleepness) player.getExtendedProperties("sleepness");
 	}
+	
+//	@Override
+//	public void init(Entity entity, World world)
+//	{
+//		this.player.getDataWatcher().addObject(20, this.max);
+//	}
 }
--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/dynamic/Strength.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/properties/dynamic/Strength.java
@@ -1,29 +1,30 @@
 package net.ci010.attributesmod.properties.dynamic;
 
+import net.ci010.attributesmod.Resource;
 import net.ci010.attributesmod.properties.Status;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IExtendedEntityProperties;
 
 public class Strength extends Status implements IExtendedEntityProperties
 {
-	// NAME is used to register extended property
-	public static final String NAME = "Strength";
-
 	public Strength(EntityPlayer player)
 	{
 		super(player);
-		this.id = "Strength";
-		this.dataId = 21;
-		this.player.getDataWatcher().addObject(dataId, this.max);
-	}
-
-	public static final void register(EntityPlayer player)
-	{
-		player.registerExtendedProperties(NAME, new Strength(player));
+		this.id = "strength";
+		this.max = Resource.maxOfSt;
 	}
 
 	public static final Strength get(EntityPlayer player)
 	{
-		return (Strength) player.getExtendedProperties(NAME);
+		return (Strength) player.getExtendedProperties("strength");
 	}
+	
+//	@Override
+//	public void init(Entity entity, World world)
+//	{
+//		this.player.getDataWatcher().addObject(21, this.max);
+//	}
 }
