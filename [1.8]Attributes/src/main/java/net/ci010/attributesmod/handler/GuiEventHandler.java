bb06af4c14a4444545af1e4195599580a8fe1ac7
ci010 <18211182378@163.com>
2015-06-27 15:26:20 +0800

    fix the achiv bug

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/GuiEventHandler.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/GuiEventHandler.java
@@ -1,11 +1,7 @@
 package net.ci010.attributesmod.handler;
 
 import net.ci010.attributesmod.gui.AttributeInventory;
-import net.ci010.attributesmod.network.PacketDispatcher;
-import net.ci010.attributesmod.network.SyncAttributesMessage;
 import net.minecraft.client.gui.inventory.GuiInventory;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/KeybindingHandler.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/KeybindingHandler.java
@@ -5,31 +5,44 @@ import net.ci010.attributesmod.network.OpenGuiMessage;
 import net.ci010.attributesmod.network.PacketDispatcher;
 import net.ci010.attributesmod.properties.Attributes;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraft.network.play.client.C16PacketClientStatus;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
 public class KeybindingHandler
 {
-	public static KeyBinding attributeMenu = new KeyBinding("Attribute Menu",21,"Attributes");
+//	public static KeyBinding attributeMenu = new KeyBinding("Smash ctrl", 21, "Attributes");
 
 	public KeybindingHandler()
 	{
-		ClientRegistry.registerKeyBinding(attributeMenu);
+//		ClientRegistry.registerKeyBinding(attributeMenu);
 	}
-	
+
 	@SubscribeEvent
-	public void keyDown(InputEvent.KeyInputEvent event)  
+	public void keyDown(InputEvent.KeyInputEvent event)
 	{
-		if(Minecraft.getMinecraft().gameSettings.keyBindInventory.isPressed())
+		if (Minecraft.getMinecraft().gameSettings.keyBindInventory.isPressed())
 		{
 			EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-			//this works...... yet I want a better solution...
-			if(player.getEntityData().getCompoundTag("ATTRIBUTES").getInteger(Attributes.power.id)==0)
+			// this works...... yet I want a better solution...
+			if (Attributes.getNBTData(player).getInteger(Attributes.power.id) == 0)
 				PacketDispatcher.sendToServer(new OpenGuiMessage(0));
-			player.openGui(AttributesMod.instance, 0, player.worldObj, (int)player.posX, (int)player.posY, (int)player.posZ);
+			
+			Minecraft.getMinecraft().getNetHandler().addToSendQueue(new C16PacketClientStatus(
+					C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT));
+			
+			player.openGui(AttributesMod.instance, 0, player.worldObj, (int) player.posX, (int) player.posY, (int) player.posZ);
+		}
+
+		if (Minecraft.getMinecraft().gameSettings.keyBindSneak.isPressed())
+		{
+			if (Minecraft.getMinecraft().gameSettings.keyBindAttack.isPressed())
+			{
+				// TODO asm to get pressed time
+				// PlayerTickHandler.trackPlayer(Minecraft.getMinecraft().thePlayer);
+			}
+
 		}
 	}
 }
