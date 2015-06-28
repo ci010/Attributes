814997dfca303dd35a16c7e24b0dc5fc445e4288
ci010 <18211182378@163.com>
2015-06-28 17:02:52 +0800

    del those useless methods

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/Resource.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/Resource.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class Resource
 {
@@ -22,45 +21,15 @@ public class Resource
 		
 		String sleepness = "Sleepness";
 		maxOfSl = config.getInt("max", sleepness, 100, 50, 300, "use to set the max value");
-		speedOfSlReg = config.getInt("regeration speed", sleepness, 20, 50, 300, "use to set the speed ");
-		speedOfSlCos = config.getInt("consumption speed", sleepness, 30, 50, 300, "use to set the speed ");
+		speedOfSlReg = config.getInt("regeration speed", sleepness, 10, 1, 30, "use to set the speed ");
+		speedOfSlCos = config.getInt("consumption speed", sleepness, 15, 1, 25, "use to set the speed ");
 		
 		String strength = "Strength";
 		maxOfSt = config.getInt("max", strength, 100, 50, 300, "use to set the max value");
-		speedOfStReg = config.getInt("regeration speed", strength, 20, 50, 300, "use to set the speed ");
-		speedOfStCos = config.getInt("consumption speed", strength, 30, 20, 300, "use to set the speed ");
+		speedOfStReg = config.getInt("regeration speed", strength, 10, 1, 30, "use to set the speed ");
+		speedOfStCos = config.getInt("consumption speed", strength, 15, 1, 25, "use to set the speed ");
 		
 		config.save();
-		//there is no config file now though...
-	}
-	
-	public static int getCommonFactor()
-	{
-//		config.get("Factor", "CommonFactor", 10);
-		//TODO need change default value
-		return 0;
 	}
 
-	public static int getHighFactor()
-	{
-//		config.get("Factor", "HighFactor", 20);
-		// TODO need change default value
-		return 0;
-	}
-
-	public static int getInitalValue()
-	{
-		// TODO 
-		return 500;
-	}
-
-	public static int getPowerFactor()
-	{
-		return 1;
-	}
-
-	public static float getFactor(String id)
-	{
-		return 0;
-	}
 }
