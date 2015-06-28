8dc45d74df47e3fb189a7db1ba587934b3ec7ada
ci010 <18211182378@163.com>
2015-06-28 17:04:07 +0800

    Calcuate the performance value when generate attributes.
    Create set methods

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/TalentHandler.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/TalentHandler.java
@@ -1,8 +1,15 @@
 package net.ci010.attributesmod.handler;
 
 import net.ci010.attributesmod.Resource;
+import net.ci010.attributesmod.network.PacketDispatcher;
+import net.ci010.attributesmod.network.SyncPlayerDataMessage;
 import net.ci010.attributesmod.properties.Attributes;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
@@ -11,7 +18,14 @@ public class TalentHandler
 {
 	@SubscribeEvent(priority=EventPriority.HIGH)
 	public void onPlayerLogin(PlayerLoggedInEvent event)
+//	public void playerConstr(EntityJoinWorldEvent event)
 	{
+		System.out.println("catch the login event");
+		if(event.player instanceof EntityPlayerMP)
+			System.out.println("lg event fires with mp");
+		if(event.player instanceof EntityPlayerSP)
+			System.out.println("lg event fires with sp");
+		
 		NBTTagCompound playerData = event.player.getEntityData();
 		
 		if(!playerData.hasKey("Talent"))
@@ -53,42 +67,67 @@ public class TalentHandler
 			attr.setInteger(Attributes.power.id, sum[2]);
 			
 			playerData.setTag("ATTRIBUTES", attr);
+			
+			NBTTagCompound per = new NBTTagCompound();
+			
+			per.setFloat(Attributes.endurance.id, Attributes.endurance.transformToPerformance(sum[0]));
+			per.setFloat(Attributes.agility.id, Attributes.agility.transformToPerformance(sum[1]));
+			per.setFloat(Attributes.power.id, Attributes.power.transformToPerformance(sum[2]));
+			
+			playerData.setTag("Performance", per);
 		}
+		
+		PacketDispatcher.sendTo(new SyncPlayerDataMessage((EntityPlayer) event.player),
+				(EntityPlayerMP) event.player);
+		
 	}
 	
 	private static int[] generateInitValue()
 	{
 		int sum = (int) (Resource.r.nextGaussian() * 5d) + 20;
 
-		int i = Resource.r.nextInt(sum) + 1;
+		int i = Resource.r.nextInt(sum);
 
-		int j = Resource.r.nextInt(sum - i) + 1;
+		int j = Resource.r.nextInt(sum - i);
 
 		int k = sum - i - j;
 
-		return new int[] { i, j, k };
+		return new int[] { i+11, j+11, k+11 };
 	}
 	
 	private static int[] generateLimitValue()
 	{
 		int sum = (int)(Resource.r.nextGaussian()*20d)+200;
 		
-		int i = Resource.r.nextInt(sum) + 1;
+		int i = Resource.r.nextInt(sum);
 
-		int j = Resource.r.nextInt(sum - i) + 1;
+		int j = Resource.r.nextInt(sum - i);
 
 		int k = sum - i - j;
 		
-		return new int[] { i, j, k };
+		return new int[] { i+101, j+101, k+101 };
 	}
 	
-	public static NBTTagCompound getTalent(NBTTagCompound playerData)
+
+	public static NBTTagCompound getTalent(EntityPlayer player)
 	{
-		return playerData.getCompoundTag("Talent");
+		return player.getEntityData().getCompoundTag("Talent");
 	}
 	
-	public static NBTTagCompound getLimit(NBTTagCompound playerData)
+	public static NBTTagCompound getLimit(EntityPlayer player)
 	{
-		return playerData.getCompoundTag("Limit");
+		return player.getEntityData().getCompoundTag("Limit");
 	}
-}
+
+	public static void setTalent(EntityPlayer player, NBTTagCompound data)
+	{
+		NBTTagCompound playerData = player.getEntityData();
+		playerData.setTag("Talent", data.getCompoundTag("Talent"));
+	}
+
+	public static void setLimit(EntityPlayer player, NBTTagCompound data)
+	{
+		NBTTagCompound playerData = player.getEntityData();
+		playerData.setTag("Limit", data.getCompoundTag("Limit"));
+	}
+}  
