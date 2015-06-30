9bfc2a1122363e5df1a8562595023698889438da
ci010 <18211182378@163.com>
2015-07-01 01:14:34 +0800

    reconstruct

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/DebugHandler.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/handler/DebugHandler.java
@@ -1,9 +1,11 @@
 package net.ci010.attributesmod.handler;
 
+import net.ci010.attributesmod.properties.Attributes;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.StatBase;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.DamageSource;
@@ -14,36 +16,35 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class DebugHandler
 {
-	@SubscribeEvent(priority=EventPriority.LOW)
+	@SubscribeEvent(priority = EventPriority.LOW)
 	public void entityHurt(LivingHurtEvent event)
 	{
 		DamageSource source = event.source;
 
 		Entity victim = event.entity;
 		Entity inflictor = source.getEntity();
-		
+
 		if (victim instanceof EntityPlayer)
+		{
 			System.out.println("player is attacked");
-		if(inflictor instanceof EntityPlayer)
+			System.out.println("dmg is " + event.ammount);
+		}
+		if (inflictor instanceof EntityPlayer)
+		{
 			System.out.println("player attacks");
-		System.out.println("dmg is " + event.ammount);
+			System.out.println("dmg is " + event.ammount);
+		}
 	}
-	
-	@SubscribeEvent(priority=EventPriority.LOW)
+
+	@SubscribeEvent(priority = EventPriority.LOW)
 	public void jumpEvent(LivingJumpEvent event)
 	{
-		StatBase distance = StatList.distanceSprintedStat;
-		if(event.entityLiving instanceof EntityPlayerSP)
+		// StatBase distance = StatList.distanceSprintedStat;
+		if (event.entityLiving instanceof EntityPlayer)
 		{
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
 		}
-		if(event.entityLiving instanceof EntityPlayerMP)
-		{
-			EntityPlayer player = (EntityPlayer) event.entityLiving;
-		}
-		
 
 	}
-	
 
 }
