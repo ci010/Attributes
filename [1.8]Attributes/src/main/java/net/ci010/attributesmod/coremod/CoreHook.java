51d2f77fdc2be97b2995b1e5eabaa73de541e989
ci010 <18211182378@163.com>
2015-07-20 15:59:02 +0800

    make a hook to calculate the airspeed

--- [1.8]Attributes/src/main/java/net/ci010/attributesmod/coremod/CoreHook.java
+++ [1.8]Attributes/src/main/java/net/ci010/attributesmod/coremod/CoreHook.java
@@ -1,10 +1,7 @@
 package net.ci010.attributesmod.coremod;
 
 import net.ci010.attributesmod.Resource;
-import net.ci010.attributesmod.handler.PlayerData;
 import net.ci010.attributesmod.properties.dynamic.Strength;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class CoreHook
@@ -21,32 +18,9 @@ public class CoreHook
 		return true;
 	}
 
-	public static void voidMethod()
-	{
-	}
-
-	public static void test()
-	{
-		// boolean a = false;
-		// boolean b = true;
-		if (getFalse() || getTrue())
-			return;
-	}
-
 	public static boolean shouldPlayerSwing(EntityPlayer player)
 	{
 		return Strength.get(player).getCurrent() <= Resource.speedOfStCos ? false : true;
-	}
-
-	public static float setSpeedInAir(EntityPlayer player, float value)
-	{
-		voidMethod();
-		return 0;
-	}
-
-	static float test(float f)
-	{
-		return f;
 	}
 
 	public static float getWalkSpeed(EntityPlayer player)
