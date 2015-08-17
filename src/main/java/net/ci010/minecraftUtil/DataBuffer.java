ec93097b17a657188ad995a0a8587064cdfc41ec
CI010 <18211182378@163.com>
2015-08-18 00:32:47 +0800

    remove useless class. relocate the network util

---
+++ src/main/java/net/ci010/minecraftUtil/DataBuffer.java
@@ -0,0 +1,50 @@
+package net.ci010.minecraftUtil;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * @author CIJhn
+ *
+ */
+public abstract class DataBuffer implements Runnable
+{
+	protected int time;
+
+	public DataBuffer()
+	{
+		time = 100;
+	}
+
+	/**
+	 * @param time
+	 */
+	public DataBuffer(int time)
+	{
+		this.time = time;
+	}
+
+	@Override
+	public void run()
+	{
+		waitTime();
+		go();
+	}
+	
+	
+	/**
+	 * wait for the time this DataBuffer created
+	 */
+	public void waitTime()
+	{
+		try
+		{
+			Thread.sleep(time);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public abstract void go();
+}
