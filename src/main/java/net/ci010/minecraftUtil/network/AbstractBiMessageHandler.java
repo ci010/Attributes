413f92f1d7202185d72b63a9e83f5441d45d8fc2
CI010 <18211182378@163.com>
2015-08-18 00:39:03 +0800

    new biMessage

---
+++ src/main/java/net/ci010/minecraftUtil/network/AbstractBiMessageHandler.java
@@ -0,0 +1,8 @@
+package net.ci010.minecraftUtil.network;
+
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+
+public abstract class AbstractBiMessageHandler<T extends IMessage> extends AbstractMessageHandler<T>
+{
+
+}
